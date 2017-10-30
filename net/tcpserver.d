a07a3f3e185eeeee3f3f899cc5d4acc6b25a0741
Eko Wahyudin <eko.wahyudin@yahoo.co.id>
2017-10-30 22:56:09 +0700

    add module TcpServer and array

--- net/tcpserver.d
+++ net/tcpserver.d
@@ -1,9 +1,39 @@
 ﻿module denpasar.net.tcpserver;
 
 import denpasar.base.classes;
+import denpasar.kernel;
+import denpasar.net.listener;
+import denpasar.net.tcpserver;
 
 class TcpServer : Activable {
+
+	void opOpAssign(string op)(Listener listener) if(op=="~") {
+		_listeners ~= listener;
+	}
+
+	void opOpAssign(string op)(Listener listener) if(op=="-") {
+		removeItemSet(_listeners, listener);
+	}
+
 protected:
 
+	override void rawActivate() 
+	{
+		foreach(Listener listener; _listeners)
+		{
+			listener.isActive = true;
+		}
+	}
+
+	override void rawDeactivate()
+	{
+		foreach(Listener listener; _listeners)
+		{
+			listener.isActive = false;
+		}
+	}
+
+private:
+	Listener[] _listeners;
 }
 
---
+++ utils/array.d
@@ -0,0 +1,21 @@
+﻿module denpasar.utils.array;
+
+import std.traits;
+
+void removeItemSet(T)(ref T[] list, T item)
+{
+	sizediff_t j = list.length - 1;
+	for(sizediff_t i=j; i>=0; i--)
+	{
+		if( list[i] == item )
+		{
+			list[i] = list[j];
+			static if( isPointer!T )
+			{
+				list[j] = null;
+			}
+			list.length = j;
+			break;
+		}
+	}
+}
